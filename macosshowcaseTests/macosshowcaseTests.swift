94ff0158f11762bf64cd3b42274f9ca409782060
Oeng Mengthong <mengthongoenggl@gmail.com>
2025-07-18 14:24:45 +0700

    Initial Commit

---
+++ macosshowcaseTests/macosshowcaseTests.swift
@@ -0,0 +1,17 @@
+//
+//  macosshowcaseTests.swift
+//  macosshowcaseTests
+//
+//  Created by Mengthong on 18/7/25.
+//
+
+import Testing
+@testable import macosshowcase
+
+struct macosshowcaseTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
